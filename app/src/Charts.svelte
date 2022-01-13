c6d566e8ba5c8df9234e0ac90f4384682c577850
Dmitriy <semchencov.dima@gmail.com>
2022-01-14 00:45:28 +0300

    Added view

--- app/src/Charts.svelte
+++ app/src/Charts.svelte
@@ -5,43 +5,20 @@
   import {faTrash, faPlus, faArrowLeft} from '@fortawesome/free-solid-svg-icons';
   import {beforeUpdate, afterUpdate} from 'svelte';
   import {slide} from 'svelte/transition';
-  import Color from 'color';
+  import ColorScheme from 'color-scheme';
   import { chart } from "svelte-apexcharts";
-  import Chart from 'svelte-frappe-charts';
-
-  let data = {
-    labels: ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'],
-    datasets: [
-      {
-        values: [10, 12, 3, 9, 8, 15, 9]
-      }
-    ]
-  };
+  import {writable} from 'svelte/store';
 
   const DATA_COUNT = 7;
   const NUMBER_CFG = {count: DATA_COUNT, min: -100, max: 100};
   let years = 10;
-
-  let options = {
-    chart: {
-      type: "bar",
-    },
-    series: [
-      {
-        name: "sales",
-        data: [30, 40, 35, 50, 49, 60, 70, 91, 125],
-      },
-    ],
-    xaxis: {
-      categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
-    },
-  };
+  let options;
 
   function calcComplexPercent(startSum, percent){
     let factor = percent / 100 + 1;
     let points = []
     for (let year of getYears(years)) {
-      points.push(startSum);
+      points.push(Math.floor(startSum));
       startSum *= factor;
     }
     return points;
@@ -58,47 +35,90 @@
       return `hsl(${h},${s}%,${l}%)`
   };
 
-  let inputData = [
+  let savedData  = JSON.parse(localStorage.getItem('data')) || [
     {name: "ОФЗ", percent: 8, color: getRandomColor(), start: 100_000},
     {name: "Акции", percent: 15, color: getRandomColor(), start: 50_000},
     {name: "Опционы", percent: 30, color: getRandomColor(), start: 20_000},
     {name: "Облигации", percent: 10, color: getRandomColor(), start: 100_000},
   ];
+
+  let inputData = writable(savedData);
+  inputData.subscribe(update => localStorage.setItem('data', JSON.stringify(update)));
+  const zip = (a, b) => a.map((k, i) => [k, b[i]]);
+
+  const scheme =  new ColorScheme;
+  scheme
+    .from_hue(randomInt(30, 200))
+    .scheme('triade')
+    .variation('soft');
+  const colors = scheme.colors().map(color => "#" + color);
+
   function getYears(amount) {
     let years = []
-    let currentYear = (new Date()).getFullYear();
+    let d = new Date();
+    let year = d.getFullYear();
+    let month = d.getMonth();
+    let day = d.getDay();
+
     for (let i of Array(amount).keys()) {
-      years.push(currentYear + i);
+      years.push((new Date(year + i, month, day)).valueOf());
     }
     return years
   }
   function recalculate() {
-    let data = {
-      labels: getYears(years),
-      datasets: [],
-      responsive: true,
+
+    let options = {
+      series: [],
+      chart: {
+        type: 'area',
+        height: 350,
+        stacked: true,
+        colors,
+        stroke: {
+          curve: 'smooth'
+        },
+        fill: {
+          type: 'gradient',
+          gradient: {
+            opacityFrom: 0.6,
+            opacityTo: 0.8,
+          }
+        },
+      },
+      dataLabels: {
+        enabled: false
+      },
+      xaxis: {
+        type: 'datetime'
+      },
+      tooltip: {
+        x: {
+          format: 'yyyy'
+        },
+      },
+      title: {
+        text: 'Расчет сложного процента',
+        align: 'center'
+      },
     }
 
-    for (let instrument of inputData) {
-      data.datasets.push({
-        fill: true,
-        lineTension: 0.3,
-        borderJoinStyle: 'miter',
-        label: instrument.name,
-        borderColor: instrument.color,
-        backgroundColor: Color(instrument.color).fade(0.2).string(),
-        pointBorderColor: instrument.color,
-        pointBackgroundColor: instrument.color,
-        data: calcComplexPercent(instrument.start, instrument.percent)
+
+    for (let instrument of $inputData) {
+
+      let data = zip(getYears(years), calcComplexPercent(instrument.start, instrument.percent))
+      options.series.push({
+        name: instrument.name,
+        data
       });
     }
-    return data
+    return options
   }
   //
-  // $: {
-  //   years, inputData;
-  //   data = recalculate();
-  // }
+  $: {
+    years, $inputData;
+    console.log('updated')
+    options = recalculate();
+  }
 
   let scrollList;
   let autoscroll
@@ -114,10 +134,9 @@
 <div class="grid place-items-center h-full">
   <div in:fade={{delay: 300}} class="w-9/12 max-w-screen-lg flex-col">
     <a class="btn btn-outline" href="/" use:link><Fa icon={faArrowLeft} style="margin-right: 5px"/>Назад</a>
-<!--    <Line {data} options={{scales: {x: {stacked: true,}, y: {stacked: true}}}}/>-->
-    <div use:chart={options}></div>
-    <Chart data={data} type="line" />
-
+    {#key options}
+      <div use:chart={options}></div>
+    {/key}
     <div class="flex justify-center my-4">
       <button class="btn btn-outline btn-sm mx-2" on:click={() => years--}>Удалить год</button>
       <button class="btn btn-outline btn-sm mx-2" on:click={() => years++}>Добавить год</button>
@@ -129,7 +148,7 @@
         <span class="p-3 badge badge-md">Процент годовых</span>
       </div>
       <div bind:this={scrollList} class="flex flex-col items-center overflow-y-auto w-full h-80">
-      {#each inputData as {name, percent, start}, i}
+      {#each $inputData as {name, percent, start}, i}
         <div class="flex flex-row my-3 space-x-7" transition:slide|local>
           <input placeholder="Название инструмента" bind:value={name} class="mx-2 input shrink input-bordered">
           <input placeholder="Стартовая сумма" bind:value={start} class="mx-2 input shrink input-bordered">
@@ -137,14 +156,14 @@
             <input placeholder="11" bind:value={percent} class="input input-bordered w-16">
             <span>%</span>
           </label>
-          <button class="btn btn-outline mx-1" on:click={() => {inputData.splice(i, 1); inputData = inputData}}>
+          <button class="btn btn-outline mx-1" on:click={() => {$inputData.splice(i, 1); $inputData = $inputData}}>
             <Fa icon={faTrash} />
           </button>
         </div>
       {/each}
       </div>
       <button class="btn btn-sm mx-auto" on:click={
-        () => inputData = inputData.concat({name: "", start: 0, percent: 0, color: getRandomColor()})
+        () => $inputData = $inputData.concat({name: "", start: 0, percent: 0, color: getRandomColor()})
       }>
         <Fa icon={faPlus}/>
       </button>
